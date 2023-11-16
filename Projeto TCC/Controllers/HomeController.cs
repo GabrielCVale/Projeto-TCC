26ecd28631075b2a042844b06860d1028ed769e2
GabrielCVale <valegabriel2@gmail.com>
2023-11-16 19:03:19 -0300

    .

---
+++ Projeto TCC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_TCC.Models;
+using System.Diagnostics;
+
+namespace Projeto_TCC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        [Route("Home/Validar")]
+        public ActionResult Validar(string email, string senha)
+        {
+            if (email == "a" || senha == "a")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projeto TCC/Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Projeto_TCC.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+
+        
+    }
+}
